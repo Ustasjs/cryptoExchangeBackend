fbd2ac65cd357c35c14bf3829301f15c187a40c8
Stanislav Ustiuzhanin <ov.s.ust@gmail.com>
2019-03-09 12:00:47 +0300

    add jwt strategy

---
+++ config/passportConfig.js
@@ -0,0 +1,62 @@
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const User = mongoose.model("user");
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+const params = {
+  secretOrKey: process.env.SEKRET_KEY,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  passReqToCallback: true
+};
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  "loginUsers",
+  new LocalStrategy(
+    {
+      usernameField: "email"
+    },
+    (username, password, done) => {
+      User.findOne({ email: username })
+        .then(user => {
+          if (user && user.validPassword(password)) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        })
+        .catch(err => {
+          done(err);
+        });
+    }
+  )
+);
+
+var strategy = new JwtStrategy(params, function(req, payload, done) {
+  User.findById(payload.id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        return done(null, user);
+      } else {
+        return done(new Error("User not found"), null);
+      }
+    })
+    .catch(err => {
+      done(err);
+    });
+});
+
+passport.use(strategy);
---
+++ controllers/userToken.js
@@ -0,0 +1,23 @@
+const jwt = require("jwt-simple");
+const passport = require("passport");
+
+module.exports.getToken = (req, res, next) => {
+  passport.authenticate("loginUsers", (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({ message: "Email or password is invalid" });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      var payload = {
+        id: user.id
+      };
+      var token = jwt.encode(payload, process.env.SEKRET_KEY);
+      res.json({ jwt: token });
+    });
+  })(req, res, next);
+};
