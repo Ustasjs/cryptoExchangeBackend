37387252c845c38809d37156385465fe5e3b2cd8
Stanislav Ustiuzhanin <ov.s.ust@gmail.com>
2019-03-27 15:57:49 +0300

    add frontend

---
+++ public/precache-manifest.283843d102cc5f092c318f84e9bfc855.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "a094807f29ad72a2a51a8e4d2ce30316",
+    "url": "/static/media/roboto-regular-webfont.a094807f.woff2"
+  },
+  {
+    "revision": "65ea671ba4ed83701341",
+    "url": "/static/css/main.c658c51a.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8707744b346888375b11060f27576c61",
+    "url": "/static/media/pattern.8707744b.svg"
+  },
+  {
+    "revision": "f274540a4718035160a0",
+    "url": "/static/js/2.7654d62d.chunk.js"
+  },
+  {
+    "revision": "2695e6d97e191976efc5dae8574501f0",
+    "url": "/static/media/Logo-white.2695e6d9.svg"
+  },
+  {
+    "revision": "4d8cc19601928a7cf92805c2ce01d097",
+    "url": "/static/media/Logo.4d8cc196.svg"
+  },
+  {
+    "revision": "0d58b18f8976bfc3b52f37ded65781fa",
+    "url": "/static/media/user-shape.0d58b18f.svg"
+  },
+  {
+    "revision": "d8495930b587b16590d4635e4772b935",
+    "url": "/static/media/padlock-unlock.d8495930.svg"
+  },
+  {
+    "revision": "c71a7ec98c45ce772b0dc3a001b5d822",
+    "url": "/static/media/rotateIcon.c71a7ec9.svg"
+  },
+  {
+    "revision": "5a5603c8ab77ba5e21069121b4ca51f7",
+    "url": "/static/media/roboto-bold-webfont.5a5603c8.woff2"
+  },
+  {
+    "revision": "f62c0213f7a7722aec7cd8102e15b721",
+    "url": "/static/media/roboto-medium-webfont.f62c0213.woff2"
+  },
+  {
+    "revision": "65ea671ba4ed83701341",
+    "url": "/static/js/main.8efb4a10.chunk.js"
+  },
+  {
+    "revision": "e319280e06910fefd3f7e1890f8bcc7b",
+    "url": "/static/media/roboto-light-webfont.e319280e.woff2"
+  },
+  {
+    "revision": "99693cfcf29eb134c04a2addcf9501b1",
+    "url": "/static/media/roboto-bold-webfont.99693cfc.woff"
+  },
+  {
+    "revision": "560a02048b6a515046c2b60941a6ff5a",
+    "url": "/static/media/roboto-medium-webfont.560a0204.woff"
+  },
+  {
+    "revision": "84d8bd90eb9adff5a3cc5c277b91eb74",
+    "url": "/static/media/roboto-regular-webfont.84d8bd90.woff"
+  },
+  {
+    "revision": "983333d9482cc5816e986de8d40fd5e1",
+    "url": "/static/media/roboto-light-webfont.983333d9.woff"
+  },
+  {
+    "revision": "e22062b3188c8199283ef2aa835d4653",
+    "url": "/static/media/Roboto-Light.e22062b3.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "f274540a4718035160a0",
+    "url": "/static/css/2.69f90c9b.chunk.css"
+  },
+  {
+    "revision": "eb8fadff82be4368e48acbca574fe02d",
+    "url": "/index.html"
+  }
+];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const path = require("path");
+const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+
+router.get("/trade/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
 });
 
 module.exports = router;
